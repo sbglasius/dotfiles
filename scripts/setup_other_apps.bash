8770afa8f52586d82fd7405108e5ab14625ae8d8
Brian Johnsen <info@brianjohnsen.dk>
2017-09-27 09:44:30 +0200

    Removes files after install

--- scripts/setup_other_apps.bash
+++ scripts/setup_other_apps.bash
@@ -6,6 +6,7 @@ echo "Installing non-apt software..."
 echo "Installing GitKraken..."
 wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
 sudo dpkg -i gitkraken-amd64.deb
+rm gitkraken-amd64.deb
 
 
 # Slack
@@ -14,6 +15,7 @@ echo "Installing Slack..."
 slack_version=2.8.0
 wget https://downloads.slack-edge.com/linux_releases/slack-desktop-$slack_version-amd64.deb
 sudo dpkg -i slack-desktop-$slack_version-amd64.deb
+rm slack-desktop-$slack_version-amd64.deb
 
 
 # Lastpass
@@ -21,6 +23,8 @@ echo "Installing Lastpass..."
 wget https://lastpass.com/lplinux.tar.bz2
 tar xjvf lplinux.tar.bz2
 cd lplinux && ./install_lastpass.sh
+rm lplinux.tar.bz2
+rm -r lplinux
 cd ..
 
 
