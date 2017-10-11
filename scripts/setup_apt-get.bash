148f9dc47d37b703590c80415d4d17386aa51683
Søren Berg Glasius <soeren@glasius.dk>
2017-10-11 15:02:56 +0200

    Adjustments for my own use

--- scripts/setup_apt-get.bash
+++ scripts/setup_apt-get.bash
@@ -2,80 +2,91 @@
 
 echo "Installing software"
 
-echo "Updating and upgrading apt-get"
-sudo apt-get -y update
-sudo apt-get -y upgrade
+echo "Adding required apt repositories"
+sudo add-apt-repository -y ppa:hluk/copyq
+sudo add-apt-repository -y universe
+sudo add-apt-repository -y ppa:snwh/pulp
+sudo add-apt-repository -y ppa:unit193/encryption
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo add-apt-repository -y ppa:aacebedo/fasd
 
-echo "Installing..."
+# until fasd gets a zesty release
+sudo echo >> /etc/apt/sources.list.d/aacebedo-ubuntu-fasd-zesty.list "deb http://ppa.launchpad.net/aacebedo/fasd/ubuntu yakkety main"
 
-# Chrome
+
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt-get update
-sudo apt-get install -y google-chrome-stable
 
 # Sublimetext: https://www.sublimetext.com/docs/3/linux_repositories.html
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update
-sudo apt-get install -y sublime-text
 
-sudo add-apt-repository -y ppa:aacebedo/fasd
-sudo apt-get update
-sudo apt-get install -y fasd
+echo "Updating and upgrading apt"
+sudo apt -y update
+sudo apt -y upgrade
 
-sudo add-apt-repository -y ppa:hluk/copyq
-sudo apt-get update
+echo "Installing..."
+
+# nvida driver
+sudo apt install -y nvidia-375
+
+# Openssh
+sudo apt install -y openssh-server
+
+# Chrome
+sudo apt install -y google-chrome-stable
+
+sudo apt install -y sublime-text
+
+sudo apt install -y fasd
+
 sudo apt install -y copyq
 
 sudo apt install -y xclip
 
 sudo apt install -y nautilus-dropbox
 
-sudo add-apt-repository -y universe
-sudo apt-get update
-sudo apt-get install -y libgconf2-4 libnss3-1d libxss1
-sudo apt-get install -y unity-tweak-tool
+sudo apt install -y libgconf2-4 libnss3-1d libxss1
+sudo apt install -y unity-tweak-tool
 
-sudo add-apt-repository -y ppa:snwh/pulp
-sudo apt-get update
-sudo apt-get install -y paper-icon-theme
-sudo apt-get install -y paper-gtk-theme
-sudo apt-get install -y paper-cursor-theme
+sudo apt install -y paper-icon-theme
+sudo apt install -y paper-gtk-theme
+sudo apt install -y paper-cursor-theme
 
+sudo apt install -y wkhtmltopdf
 
-sudo apt-get install -y wkhtmltopdf
+#sudo apt install -y docker.io
 
-sudo apt install -y docker.io
-
-sudo apt-get install -y bleachbit
+sudo apt install -y bleachbit
 
 # Because: http://askubuntu.com/questions/856659/backup-does-not-work-on-ubuntu-16-10-and-16-04
-sudo apt-get install -y duplicity
+sudo apt install -y duplicity
 
 # Preview (http://www.omgubuntu.co.uk/2016/09/gnome-sushi-mac-quick-look-nautilus)
-sudo apt-get install -y gnome-sushi
+sudo apt install -y gnome-sushi
 
 # http://manpages.ubuntu.com/manpages/trusty/man1/xdotool.1.html
 sudo apt install -y xdotool
 
 # Ubuntu make
-sudo apt-get install -y ubuntu-make
-yes | sudo umake ide idea-ultimate
-yes | sudo umake ide datagrip
+sudo apt install -y ubuntu-make
+mkdir -p $HOME/applications/idea
+mkdir -p $HOME/applications/datagrip
+echo "$HOME/applications/idea" | umake ide idea-ultimate
+# echo "$HOME/applications/datagrip" | umake ide datagrip
 
 
 # 1. Add the Spotify repository signing key to be able to verify downloaded packages
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 # 2. Add the Spotify repository
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+#echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 # 3. Update list of available packages
-sudo apt-get update
+#sudo apt update
 # 4. Install Spotify
-sudo apt-get install -y spotify-client
+#sudo apt install -y spotify-client
 
 # Vagrant
-sudo apt install -y vagrant
+#sudo apt install -y vagrant
 
 # Power management
 sudo apt install -y tlp tlp-rdw
@@ -83,8 +94,6 @@ sudo tlp start
 
 sudo apt install -y dconf-editor
 
-sudo add-apt-repository -y ppa:unit193/encryption
-sudo apt update
 sudo apt install -y veracrypt
 
 sudo apt install -y ranger
@@ -98,12 +107,13 @@ sudo apt install -y postgresql-client8
 ########################################################################################################################
 
 # Java
-sudo add-apt-repository -y ppa:webupd8team/java
-sudo apt-get update
 echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-sudo apt-get install -y oracle-java7-installer
-sudo apt-get install -y oracle-java8-installer
+echo "Java7 will fail installation. After running the first time, go to"
+echo "http://www.oracle.com/technetwork/java/javase/downloads/java-archive-downloads-javase7-521261.html"
+echo "requires login. Download oracle-jdk7-amd64.tgz and place in /var/cache/oracle-jdk7-installer/"
+sudo apt install -y oracle-java7-installer
+sudo apt install -y oracle-java8-installer
 # Java Cryptography Extension
 sudo apt install -y oracle-java7-unlimited-jce-policy
 sudo apt install -y oracle-java8-unlimited-jce-policy
@@ -111,7 +121,7 @@ sudo apt install -y oracle-java8-unlimited-jce-policy
 
 # MySQL
 if [ -f ! /etc/init.d/mysql* ]; then
-    sudo apt-get install -y mysql-server
+    sudo apt install -y mysql-server
     # only run init script first time
     sudo mysql_secure_installation
 fi
